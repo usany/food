9b305c5d6219bfe126e11aab397d74179263918b
usany <ckd_qja@naver.com>
2026-06-11 00:45:43 +0900

    cern

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -21,16 +21,16 @@ from . import views
 
 urlpatterns = [
     path('', views.root_redirect, name='root'),
+    path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
     # path('se/', views.home, name='home'),
     # path('se/<str:path>/', views.menu_list, name='menu_list_se'),
     # path('se/<str:path>/<str:meal>/', views.menu_detail, name='menu_detail_se'),
     # path('gl/', views.home_gl, name='home_gl'),
     # path('gl/<str:path>/', views.menu_list, name='menu_list_gl'),
     # path('gl/<str:path>/<str:meal>/', views.menu_detail, name='menu_detail_gl'),
-    path('admin/', admin.site.urls),
     # path('adminpage/', views.admin_view, name='admin_view'),
     # path('pages/', include('pages.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
     # Generic language/campus route - MUST BE LAST to avoid catching other routes
     # path('<str:base>/<str:bases>/', views.home_menu, name='home_menu'),
     # path('<str:base>/<str:bases>/<str:path>/', views.menu_list, name='home_menu'),
