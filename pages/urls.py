cf353d0d64042ddc89c297060de709fbfbb61efa
usany <ckd_qja@naver.com>
2026-06-11 00:35:32 +0900

    cern

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, i18n_patterns
 from . import views
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('', views.menu_list, name='menu_list'),
     path('<int:pk>/', views.menu_detail, name='menu_detail'),
     path('admin/', views.admin_view, name='admin_view'),
-]
+    prefix_default_language=True,
+)
 
