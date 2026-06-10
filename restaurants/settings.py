72ce4c2f35eefd150f8309f217533b192cd15c59
usany <ckd_qja@naver.com>
2026-06-11 01:32:50 +0900

    cern

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -126,9 +126,20 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ko'
 
+LANGUAGES = [
+    ('ko', 'Korean'),
+    ('en', 'English'),
+]
+
+LOCALE_PATHS = [
+    BASE_DIR / 'locale',
+]
+
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
