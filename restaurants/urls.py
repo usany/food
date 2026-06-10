62d286c7571827f2ea7c2f474d92bf67adc2e6d4
usany <ckd_qja@naver.com>
2026-06-11 00:55:54 +0900

    cern

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, i18n_patterns
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 from . import views
 
 urlpatterns = [
