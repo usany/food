4d383560fa1588b072d86be72ac03ef33d1bcda4
usany <ckd_qja@naver.com>
2026-06-15 15:32:27 +0900

    cern

--- restaurants/management/commands/playwright.py
+++ restaurants/management/commands/playwright.py
@@ -494,7 +494,7 @@ class Command(BaseCommand):
         #     return
 
         # url = f"https://objectstorage.ap-chuncheon-1.oraclecloud.com/p/{par_token}/n/{namespace}/b/{bucket}/o/{object_name}"
-        url = f"https://objectstorage.ap-chuncheon-1.oraclecloud.com/p/AHQzNfsY1ppOpv7VpX-C9iKAwqJJkyUun8n3L4R08J_OuNEQ8x9p1UKvJZMk2X_h/n/ax0ym4amgnfk/b/bucket-20260516-0145/o/{object_name}"
+        url = f"https://objectstorage.ap-chuncheon-1.oraclecloud.com/p/_jTGcQoC5j9SBp5Q9iVJ4U7y0oilMttgFhVFrPo90gFeMwUn2JQVeAjTQetS6HNh/n/ax0ym4amgnfk/b/bucket-20260516-0145/o/{object_name}"
 
         if not os.path.exists(file_path):
             self.stderr.write(self.style.ERROR(f'File not found: {file_path}'))
