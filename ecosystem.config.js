864e9ff78496d5ffdeb3a8e9092df1a69aaaf8d0
usany <ckd_qja@naver.com>
2026-06-22 20:04:24 +0900

    cern

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "food",
+      script: "python3",
+      args: "manage.py runserver 0.0.0.0:8000",
+      cwd: "/home/food",
+      interpreter: "none",
+      autorestart: true,
+      watch: false,
+      env: {
+        DJANGO_SETTINGS_MODULE: "restaurants.settings",
+      },
+    },
+  ],
+};
