0e3fff584c166ae504b1b3a5914ca4e91bef24e4
usany <ckd_qja@naver.com>
2026-06-22 20:05:22 +0900

    cern

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-h=*wyi-pqo-i2$%15#yx(!1mruj0)5y0m^nj-jve0j*_kk#i@f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['134.185.111.151','127.0.0.1', 'kie.khusan.co.kr']
 
 # Application definition
 
