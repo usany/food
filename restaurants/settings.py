6fd9607d305361d1fd1f69ff1f461d1787823ab3
usany <ckd_qja@naver.com>
2026-06-24 20:35:43 +0900

    cern

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -166,7 +166,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'restaurants' / 'static',
 ]
