b695fcdbc432124211284146d8fc7de3e1a98547
usany <ckd_qja@naver.com>
2026-06-24 17:49:46 +0900

    cern

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'pages' / 'static',
+    BASE_DIR / 'restaurants' / 'static',
 ]
 
 # LOGIN_URL = '/django-admin/login/'
