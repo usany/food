54d5094a3f9e9eeda7424e18ee084988154b3c15
usany <ckd_qja@naver.com>
2026-07-06 07:22:20 +0900

    cern

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_browser_reload',
     'restaurants.apps.RestaurantsConfig',
     'pwa',
     # 'pages'
@@ -58,7 +57,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'restaurants.urls'
